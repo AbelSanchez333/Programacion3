044e5a1520d6d15ba0bf17ab3300689536a0808e
AbelSánchez333 <asancheza9@miumg.edu.gt>
2024-01-27 12:03:33 -0600

    Se agrego el archivo de metodos

---
+++ Lab2Ejercicios/Metodos.py
@@ -0,0 +1,70 @@
+##Metodo capitalize
+texto = "hola mundo"
+resultado = texto.capitalize()
+##print(resultado)
+
+##Metodo find
+texto = "Hola Mundo"
+resultado = texto.find("Mundo")
+##print(resultado)
+
+##Metodo center (Centra el texto en un conjunto de caracteres)
+texto = "Hola Mundo"
+resultado = texto.center(20,'*')
+##print(resultado)
+
+##Metodo isalnum (Devuelve True si todos los caracteres de la cadena son alfanumericos)
+texto = "Python1234567"
+resultado = texto.isalnum()
+##print(resultado)
+
+##Metodo isdigit (Devuelve True si todos los caracteres de la cadena son digitos)
+texto = "234564"
+resultado = texto.isdigit()
+##print(resultado)
+
+##Metodo islwer (Devuelve True si todos los caracteres de la cadena estan en minuscula)
+texto = "sasdfasdfadff"
+resultado = texto.islower()
+##print(resultado)
+
+##Metodo isspace (Devuelve True si todos los caracteres de la cadena son espacios en blanco)
+texto = "       "
+resultado = texto.isspace()
+##print(resultado)
+
+##TAREA Probar los siguientes metodos: 
+#strip()
+#replace()
+#rstrip()
+#split()
+
+##Metodo strip (Elimina los espacios en blanco al inicio y final de una cadena)
+texto = "  Hola mundo     "
+resultado = texto.strip()
+##print(resultado)
+
+##Metodo replace (Remplaza una cadena especifica con otra cadena)
+texto = "Hola mundo"
+resultado = texto.replace("Hola","Que Tal")
+##print("Cadena original:",texto)
+##print("Cadena modificada:",resultado)
+
+##Metodo rstrip (Elimina los espacios en blanco del espacio derecho de una cadena)
+texto = "  Hola mundo     "
+resultado = texto.rstrip()
+##print(resultado)
+
+##Metodo split (Divide una cadena de texto en una lista subscadenas)
+texto = "Hola Amigo Como Estas?"
+resultado = texto.split()
+##print(resultado)
+
+
+
+
+
+
+
+
+
