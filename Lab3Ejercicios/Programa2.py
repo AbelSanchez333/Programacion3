587ff067f1673bd4fba5bcd171038433b65eb701
AbelSánchez333 <asancheza9@miumg.edu.gt>
2024-02-03 18:32:05 -0600

    se hicieron cambios

--- Lab3Ejercicios/Programa2.py
+++ Lab3Ejercicios/Programa2.py
@@ -1,1 +1,13 @@
 print("**CONDICIONALES Y OPERADORES RELACIONALES**")
+try:
+    numero = float(input("Ingrese un numero: "))
+    if numero < 0:
+        print("El numero que ingreso es negativo")
+    elif numero > 0:
+        print("El numero que ingreso es positivo")
+    else:
+        print("El numero que ingreso es cero")
+except ValueError:
+    print("EL valor ingresado no es valido")
+
+
--- Lab3Ejercicios/Programa3.py
+++ Lab3Ejercicios/Programa3.py
@@ -1,1 +1,8 @@
 print("**CICLOS Y OPERADORES LOGICOS**")
+print("Los primeros 10 numeros pares son: ")
+i = 0
+while i<=10:
+    porcentaje = i%2
+    if porcentaje == 0:
+        print(f"- {i}")
+    i = i + 1
