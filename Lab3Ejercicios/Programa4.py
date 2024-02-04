bdc2a2af33e7b78f590bfa546b1f828da6171eba
AbelSánchez333 <asancheza9@miumg.edu.gt>
2024-02-03 18:43:51 -0600

    Se hicieron cambios

--- Lab3Ejercicios/Programa4.py
+++ Lab3Ejercicios/Programa4.py
@@ -1,1 +1,24 @@
 print("**FUNCIONES Y CONDICIONALES**")
+
+def suma_es_par(num1, num2):
+    suma = num1 + num2
+    return suma % 2 == 0
+
+
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el segundo numero: "))
+resultado = suma_es_par(numero1, numero2)
+
+if resultado == True:
+    print (f"La suma de ambos numeros es par: {resultado}")
+else:
+    print (f"La suma de ambos numeros es impar: {resultado}")
+
+
+
+
+
+
+
+
+
